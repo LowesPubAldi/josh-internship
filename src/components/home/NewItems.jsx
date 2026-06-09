24ecede08bcebbc2dadbdb0311a0641c87a2790d
LowesPubAldi <jovanminsel@gmail.com>
2026-06-09 17:34:55 -0500

    Completed NewItems section

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import  { Link } from "react-router-dom";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import Skeleton from "../UI/Skeleton";
 
 const NewItems = () => {
+  const[items, setItems] = useState([]);
+
+  async function getNewItems () {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
+    );
+
+    setTimeout (() => {
+    setItems(data);
+  }, 2000);
+}
+
+  useEffect(() => {
+    getNewItems();
+  
+  }, []);
+  
+function timeRemaining(expiryDate) {
+  const total = expiryDate - Date.now();
+
+  if (total <= 0) {
+    return "0h 0m 0s";
+  }
+
+  const hours = Math.floor(total / (1000 * 60 * 60));
+  const minutes = Math.floor((total / (1000 * 60)) % 60);
+  const seconds = Math.floor((total / 1000) % 60);
+
+  return `${hours}h ${minutes}m ${seconds}s`;
+}
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,8 +48,30 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+{items.length === 0 ? (
+  <div className="row">
+    {new Array(4).fill(0).map((_, index) => (
+      <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+        <Skeleton width="100%" height="350px" border-radius="12px" />
+        </div>
+    ))}
+    </div>
+  ) : (
+
+  <OwlCarousel
+    className="owl-carousel owl-theme"
+    loop
+    margin={20}
+    nav
+    dots={false}
+    responsive={{
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items: 4 },
+    }}
+  >
+          {items.map((item) => (
+            <div className="item" key={item.id}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
@@ -24,11 +80,13 @@ const NewItems = () => {
                     data-bs-placement="top"
                     title="Creator: Monica Lucas"
                   >
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={item.AuthorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
+                <div className="de_countdown">
+                  {timeRemaining(item.expiryDate)}
+                  </div>
 
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
@@ -51,7 +109,7 @@ const NewItems = () => {
 
                   <Link to="/item-details">
                     <img
-                      src={nftImage}
+                      src={item.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
@@ -59,17 +117,19 @@ const NewItems = () => {
                 </div>
                 <div className="nft__item_info">
                   <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
+                  <div className="nft__item_price">{item.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
             </div>
           ))}
+           </OwlCarousel>
+)}
         </div>
       </div>
     </section>
